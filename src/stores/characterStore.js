7d0d7c76e2b13472e13518dc84981dc038997a85
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2024-11-30 22:07:36 -0300

    setup pinia and made according changes

---
+++ src/stores/characterStore.js
@@ -0,0 +1,39 @@
+import { defineStore } from "pinia";
+import { api } from "@/util/axios";
+
+const CHARACTER_URL = "/character"
+export const useCharacterStore = defineStore('characterStore',{
+    state:()=>{
+        _characters:{}
+    },
+    getters:{
+        characters: (state)=>state._characters
+    },
+    actions:{
+        async getAllCharacters(){
+            try {
+                const response = await api.get(`${CHARACTER_URL}/getall`);
+                this._characters = response.data;
+                return this.characters;
+            } catch (error) {
+                console.error("Error fetching characters:", error);
+                throw error;
+            }
+        },
+        async addNewCharacter(characterData){
+            try {
+                const body = {
+                    name: characterData.name,
+                    image: characterData.image,
+                    age: characterData.age,
+                    color: characterData.color
+                }
+
+                await api.post(`${CHARACTER_URL}/create`, body);
+                await this.getAllCharacters();
+            } catch (error) {
+                console.error("Error fetching characters:", error);
+                throw error;
+            }
+        }    }
+})
