a4bbbb1102752d11fd870410fba9f4df16e952b0
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2024-11-30 23:18:03 -0300

    starting changes to automatic tailwind

--- src/components/MessagesView.vue
+++ src/components/MessagesView.vue
@@ -1,12 +1,14 @@
 <template>
-    <CharacterMessage
-        class="mb-5"
-        v-for="(message, index) in messages"
-        :key="index"
-        :text="message.text"
-        :color="characterInfoById(message.characterId).color"
-        :characterInfo="characterInfoById(message.characterId)"
-    />
+    <div class="bg-green-500 overflow-auto h-32">
+        <CharacterMessage
+            v-for="(message, index) in messages"
+            class="mb-5"
+            :key="index"
+            :text="message.text"
+            :color="characterInfoById(message.characterId).color"
+            :characterInfo="characterInfoById(message.characterId)"
+        />
+        </div>
 </template>
 <script setup>
 import { storeToRefs } from 'pinia';
